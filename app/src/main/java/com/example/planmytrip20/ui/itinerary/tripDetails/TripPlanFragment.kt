6112e2c0ebb8731b7156e1e996a42bb349b4ab49
Tharun Patteti <tharun45@bu.edu>
2023-05-08 15:57:44 -0400

    Fix for error updating locations to trip plan fragment

--- app/src/main/java/com/example/planmytrip20/ui/itinerary/tripDetails/TripPlanFragment.kt
+++ app/src/main/java/com/example/planmytrip20/ui/itinerary/tripDetails/TripPlanFragment.kt
@@ -6,6 +6,7 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.appcompat.app.AppCompatActivity
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
 import androidx.lifecycle.ViewModelStoreOwner
@@ -34,34 +35,58 @@ class TripPlanFragment(private val viewModelOwner: ViewModelStoreOwner) : Fragme
         val itineraryViewModel =
             ViewModelProvider(viewModelOwner).get(ItineraryViewModel::class.java)
 
-        // TODO: Get all the locations that are in itinerary and pass to recyclerview
-//        locations.add(ItineraryLocation("test_id", "test_place_id", "Brighton MA 1",
-//            "50 Winship Street", LatLng(42.9912, -70.456), OpeningHours(true), true, "The contents within a card should follow their own accessibility guidelines, such as images having content descriptions set on them.", rating = 5.0))
-//
-//        locations.add(ItineraryLocation("test_id", "test_place_id", "Brighton MA 3",
-//            "50 Winship Street ", LatLng(42.9912, -72.456), OpeningHours(true), true, "The contents within a card should follow their own accessibility guidelines, such as images having content descriptions set on them.", rating = 4.0))
-//        locations.add(ItineraryLocation("test_id", "test_place_id", "Brighton MA 2 12312312312321321321321321323213213213213213213123123123213",
-//            "50 Winship Street", LatLng(42.9912, -74.456), OpeningHours(true), false, "The contents within a card should follow their own accessibility guidelines, such as images having content descriptions set on them.", rating = 3.0))
-//        locations.add(ItineraryLocation("test_id", "test_place_id", "Brighton MA 4",
-//            "50 Winship Street", LatLng(42.9912, -76.456), OpeningHours(true), false, "The contents within a card should follow their own accessibility guidelines, such as images having content descriptions set on them.", rating = 2.0))
-//        locations.add(ItineraryLocation("test_id", "test_place_id", "Brighton MA 7",
-//            "50 Winship Street", LatLng(42.9912, -78.456), OpeningHours(true), false, "The contents within a card should follow their own accessibility guidelines, such as images having content descriptions set on them.", rating = 2.0))
-//
-//        locations.add(ItineraryLocation("test_id", "test_place_id", "Brighton MA 7",
-//            "50 Winship Street", LatLng(42.9912, -80.456), OpeningHours(true), false, "The contents within a card should follow their own accessibility guidelines, such as images having content descriptions set on them.", rating = 2.0))
-
-
-        Log.d(TAG, "testing list: "+locations.size)
-
-
         itineraryViewModel.chosenPlaces.observe(viewLifecycleOwner, Observer { newLocations ->
-            locations = newLocations as MutableList<ItineraryLocation>
-            adapter.notifyDataSetChanged()
+            if(newLocations.isNotEmpty()){
+                locations = newLocations as MutableList<ItineraryLocation>
+                adapter = ChecklistRecyclerViewAdapter(itineraryViewModel, locations)
+                _binding!!.list.adapter = adapter
+
+                adapter.openMap = { loc1: ItineraryLocation, loc2: ItineraryLocation, mapType: String ->
+                    val modal = MapBottomSheetFragment()
+                    val bundle = Bundle()
+                    modal.dialog
+                    bundle.putString("source", GsonBuilder().create().toJson(loc1))
+                    bundle.putString("destination", GsonBuilder().create().toJson(loc2))
+                    bundle.putString("mapType", mapType)
+                    modal.arguments = bundle
+                    modal.show(childFragmentManager, "Map bottom sheet")
+                }
+
+                adapter.onCheckBoxChange = {position: Int, checked: Boolean ->
+                    Log.d(TAG, "Fragment onCheckBoxChange")
+                    if(checked){
+                        locations[position].visited = true
+                        if (!binding.list.isComputingLayout)
+                        {
+                            //TODO: Update the database and cache with location update
+                            adapter.notifyItemChanged(position)
+                            adapter.notifyItemChanged(position + 1)
+                            adapter.notifyItemChanged(position - 1)
+                        }
+                    } else {
+                        locations[position].visited = false
+                        if (!binding.list.isComputingLayout)
+                        {
+                            //TODO: Update the database and cache with location update
+                            adapter.notifyItemChanged(position)
+                            adapter.notifyItemChanged(position + 1)
+                            adapter.notifyItemChanged(position - 1)
+                        }
+                    }
+                }
+
+                adapter.openWebView = {wikiUrl: String ->
+                    var fragment = WebViewFragment()
+                    val transaction = (context as AppCompatActivity).supportFragmentManager.beginTransaction()
+                    transaction.replace(R.id.nav_host_fragment_activity_main, fragment)
+                    transaction.addToBackStack(null)
+                    transaction.setReorderingAllowed(true)
+                    transaction.commit()
+                }
+            }
+
             Log.d("itinerery", "onCreateView: ${locations.size} working tripPlan")
         })
-
-        adapter = ChecklistRecyclerViewAdapter(itineraryViewModel, locations)
-
 
         itineraryViewModel.destination.observe(viewLifecycleOwner, Observer {
             Log.d("itinerery", "onCreateView: " + it.address+"working tripPlan")
@@ -74,52 +99,6 @@ class TripPlanFragment(private val viewModelOwner: ViewModelStoreOwner) : Fragme
                 .setMessage("This feature is only available for PRO members. Please upgrade to premium")
                 .setPositiveButton("OK") { dialog, which ->
                 }.show()
-        }
-
-
-        _binding!!.list.adapter = adapter
-
-        adapter.openMap = { loc1: ItineraryLocation, loc2: ItineraryLocation, mapType: String ->
-            val modal = MapBottomSheetFragment()
-            val bundle = Bundle()
-            modal.dialog
-            bundle.putString("source", GsonBuilder().create().toJson(loc1))
-            bundle.putString("destination", GsonBuilder().create().toJson(loc2))
-            bundle.putString("mapType", mapType)
-            modal.arguments = bundle
-            modal.show(childFragmentManager, "Map bottom sheet")
-        }
-
-        adapter.onCheckBoxChange = {position: Int, checked: Boolean ->
-            Log.d(TAG, "Fragment onCheckBoxChange")
-            if(checked){
-                locations[position].visited = true
-                if (!binding.list.isComputingLayout)
-                {
-                    //TODO: Update the database and cache with location update
-                    adapter.notifyItemChanged(position)
-                    adapter.notifyItemChanged(position + 1)
-                    adapter.notifyItemChanged(position - 1)
-                }
-            } else {
-                locations[position].visited = false
-                if (!binding.list.isComputingLayout)
-                {
-                    //TODO: Update the database and cache with location update
-                    adapter.notifyItemChanged(position)
-                    adapter.notifyItemChanged(position + 1)
-                    adapter.notifyItemChanged(position - 1)
-                }
-            }
-        }
-
-        adapter.openWebView = {wikiUrl: String ->
-            var fragment = WebViewFragment()
-//            val transaction = childFragmentManager.beginTransaction()
-//            transaction.replace(R.id., fragment)
-//            transaction.addToBackStack(null)
-//            transaction.setReorderingAllowed(true)
-//            transaction.commit()
         }
 
         return binding.root
