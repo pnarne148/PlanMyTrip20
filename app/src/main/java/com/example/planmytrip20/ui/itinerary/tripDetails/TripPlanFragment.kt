e9a6161e371970d5d791c9258ea8505fb0ee7e16
ManiMukeshReddyNukal <114439598+ManiMukeshReddyNukal@users.noreply.github.com>
2023-05-09 12:22:23 -0400

    upload single image

--- app/src/main/java/com/example/planmytrip20/ui/itinerary/tripDetails/TripPlanFragment.kt
+++ app/src/main/java/com/example/planmytrip20/ui/itinerary/tripDetails/TripPlanFragment.kt
@@ -1,10 +1,19 @@
 package com.example.planmytrip20.ui.itinerary.tripDetails
 
+import android.app.Activity
+import android.content.Context
+import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
+import android.provider.MediaStore
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.activity.result.ActivityResultLauncher
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.appcompat.app.AppCompatActivity
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
 import androidx.lifecycle.ViewModelStoreOwner
@@ -13,35 +22,74 @@ import com.example.planmytrip20.classes.ItineraryLocation
 import com.example.planmytrip20.databinding.FragmentTripPlanBinding
 import com.example.planmytrip20.ui.itinerary.ItineraryViewModel
 import com.example.planmytrip20.ui.itinerary.maps.MapBottomSheetFragment
+import com.google.firebase.storage.FirebaseStorage
+import com.google.firebase.storage.StorageReference
 import com.google.gson.GsonBuilder
+import java.util.*
+import kotlin.collections.ArrayList
 
 class TripPlanFragment(private val viewModelOwner: ViewModelStoreOwner) : Fragment() {
     private var _binding: FragmentTripPlanBinding? = null
     private val binding get() = _binding!!
     val TAG = "Trip Plan Fragment"
     private lateinit var adapter: ChecklistRecyclerViewAdapter
-
+    private val storageReference: StorageReference = FirebaseStorage.getInstance().reference
+    private val STORAGE_PERMISSION_CODE = 1001
     private var locations : MutableList<ItineraryLocation> = ArrayList()
+
+    private lateinit var cameraLauncher: ActivityResultLauncher<Intent>
+
+    private fun startGalleryIntent() {
+        val galleryIntent = Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI)
+        cameraLauncher.launch(galleryIntent)
+    }
+
+    private fun uploadImageToFirebaseStorage(imageUri: Uri) {
+        val imageRef = storageReference.child("images/${UUID.randomUUID()}.jpg")
+        imageRef.putFile(imageUri)
+            .addOnSuccessListener { taskSnapshot ->
+                imageRef.downloadUrl.addOnSuccessListener { downloadUri ->
+                    Log.d(TAG,"++++++++++++++++++++++++++++")
+                    Log.d(TAG, "Image URL: $downloadUri")
+                    // Save the URL to the location object or database, and update the UI accordingly
+                }
+            }
+            .addOnFailureListener { exception ->
+                Log.e(TAG, "Failed to upload image: ${exception.message}")
+            }
+    }
+
+
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
         val itineraryViewModel =
             ViewModelProvider(viewModelOwner).get(ItineraryViewModel::class.java)
-
+        cameraLauncher = registerForActivityResult(ActivityResultContracts.StartActivityForResult()) { result ->
+            if (result.resultCode == Activity.RESULT_OK) {
+                val imageUri = result.data?.data
+                if (imageUri != null) {
+                    uploadImageToFirebaseStorage(imageUri)
+                } else {
+                    Log.e(TAG, "Image URI is null")
+                }
+            }
+        }
         val binding = FragmentTripPlanBinding.inflate(inflater, container, false)
-
         with(binding.list) {
             layoutManager = LinearLayoutManager(context)
 
             itineraryViewModel.chosenPlaces.observe(viewLifecycleOwner, Observer {newLocations ->
                 if(newLocations.isNotEmpty()) {
                     itineraryViewModel.latestIndex.observe(viewLifecycleOwner, Observer {
-                        adapter = ChecklistRecyclerViewAdapter(context, itineraryViewModel, newLocations, it)
+                        adapter = ChecklistRecyclerViewAdapter(context, itineraryViewModel, newLocations, it, ::startGalleryIntent)
                     })
                 }
             })
         }
+
+
 
 //        itineraryViewModel.chosenPlaces.observe(viewLifecycleOwner, Observer { newLocations ->
 //            if(newLocations.isNotEmpty()){
